a7527cb910e96b3a2cb90bddd52ecf80b1dcb0ca
madhukar chouhan <madhukarsingh11@gmail.com>
2019-11-04 20:30:32 +0530

    Card Addede

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -9,6 +9,12 @@ export class MainComponent implements OnInit {
 
   constructor() { }
 
+   tiles: Tile[] = [
+    {date: '26 june 2019', projectname: "Task Developer", status: "In-progress", color: 'lightblue'},
+    {date: '26 june 2019', projectname: "Task Developer", status: "In-progress", color: 'lightblue'}
+    
+  ];
+
   ngOnInit() {
   }
 
